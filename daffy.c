66d5a1718f24e4f916be9b1c37ecf9033ddc689c
Claire <clairekcecil@gmail.com>
2017-08-11 02:20:27 -0400

    Created a grammar for the interpreter and added gitignore
    The grammar recognizes numbers, operators, and expressions
    Added the mpc library for parsing
    Created the gitignore to not include the interpreter's executable in the repo

---
+++ daffy.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mpc.h"
+
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+int main(int argc, char** argv) {
+
+	/* making the parsers */
+	mpc_parser_t* Number   = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr     = mpc_new("expr");
+	mpc_parser_t* Lispy    = mpc_new("lispy");
+
+	/* define the grammar */
+	mpca_lang(MPCA_LANG_DEFAULT,
+	"                                                     \
+	number   : /-?[0-9]+/ ;                             \
+	operator : '+' | '-' | '*' | '/' | '%' | '^' | \"add\" | \"exp\" | \"mul\" | \"sub\" | \"div\" | \"mod\" ;  \
+	expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+	lispy    : /^/ <operator> <expr>+ /$/ ;             \
+	",
+	Number, Operator, Expr, Lispy);
+
+	puts("Daffy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
+
+	while(1) {
+		char* input = readline("daffy> ");
+		add_history(input);
+		printf("%s\n", input);
+		free(input);
+	}
+	return 0;
+}
