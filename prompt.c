87a3070660a7bcaf14e02122a35e94d3d7e46d49
debugduck <clairekcecil@gmail.com>
2022-06-18 15:09:38 -0400

    add exp as an accepted alternative symbol for ^

--- prompt.c
+++ prompt.c
@@ -28,7 +28,7 @@ long eval_op(long x, char* op, long y) {
 	if(strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) { return x * y; }
 	if(strcmp(op, "/") == 0 || strcmp(op, "div") == 0) { return x / y; }
 	if(strcmp(op, "%") == 0 || strcmp(op, "mod") == 0) { return x % y; }
-	if(strcmp(op, "^") == 0) { return pow(x, y); }
+	if(strcmp(op, "^") == 0 || strcmp(op, "exp") == 0) { return pow(x, y); }
 	return 0;
 }
 
@@ -82,7 +82,7 @@ int main(int argc, char** argv) {
 	mpca_lang(MPCA_LANG_DEFAULT,
 	"                                                     \
 	number   : /-?[0-9]+/ ;                             \
-	operator : '+' | '-' | '*' | '/' | '%' | '^' |\"add\" | \"exp\" | \"mul\" | \"sub\" | \"div\" | \"mod\" ;  \
+	operator : '+' | '-' | '*' | '/' | '%' | '^' |\"add\" | \"exp\" | \"mul\" | \"sub\" | \"div\" | \"mod\" | \"exp\";  \
 	expr     : <number> | '(' <operator> <expr>+ ')' ;  \
 	lispy    : /^/ <operator> <expr>+ /$/ ;             \
 	",
