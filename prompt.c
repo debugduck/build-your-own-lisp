aae90589450587aa6eb32adc4794aa5cb6c2f8ce
debugduck <clairekcecil@gmail.com>
2022-06-18 18:03:18 -0400

    add max function

--- prompt.c
+++ prompt.c
@@ -30,6 +30,7 @@ long eval_op(long x, char* op, long y) {
 	if(strcmp(op, "%") == 0 || strcmp(op, "mod") == 0) { return x % y; }
 	if(strcmp(op, "^") == 0 || strcmp(op, "exp") == 0) { return pow(x, y); }
 	if(strcmp(op, "min") == 0) { return x <= y ? x : y; }
+	if(strcmp(op, "max") == 0) { return x >= y ? x : y; }
 	return 0;
 }
 
@@ -83,7 +84,7 @@ int main(int argc, char** argv) {
 	mpca_lang(MPCA_LANG_DEFAULT,
 	"                                                     \
 	number   : /-?[0-9]+/ ;                             \
-	operator : '+' | '-' | '*' | '/' | '%' | '^' |\"add\" | \"exp\" | \"mul\" | \"sub\" | \"div\" | \"mod\" | \"exp\" | \"min\";  \
+	operator : '+' | '-' | '*' | '/' | '%' | '^' |\"add\" | \"exp\" | \"mul\" | \"sub\" | \"div\" | \"mod\" | \"exp\" | \"min\" | \"max\";  \
 	expr     : <number> | '(' <operator> <expr>+ ')' ;  \
 	lispy    : /^/ <operator> <expr>+ /$/ ;             \
 	",
